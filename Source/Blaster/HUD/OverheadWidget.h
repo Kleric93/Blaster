5a1626e70844c5b088467d046b2bb18af5fcd614
Kleric93 <121462973+Kleric93@users.noreply.github.com>
2023-02-12 15:34:57 +0100

    Lecture43_2 Network Role (also challenge for player name)
    https://www.udemy.com/course/unreal-engine-5-cpp-multiplayer-shooter/learn/lecture/31456500#questions/18736820

--- Source/Blaster/HUD/OverheadWidget.h
+++ Source/Blaster/HUD/OverheadWidget.h
@@ -13,5 +13,20 @@ UCLASS()
 class BLASTER_API UOverheadWidget : public UUserWidget
 {
 	GENERATED_BODY()
+
+public:
+	UPROPERTY(meta = (BindWidget))
+	class UTextBlock* DisplayText;
+
+	void SetDisplayText(FString TextToDisplay);
+
+	UFUNCTION(BlueprintCallable)
+	void ShowPlayerNetRole(APawn* InPawn);
+
+	UFUNCTION(BlueprintCallable)
+	void ShowPlayerName(APawn* InPawn);
+
+protected:
+void OnLevelRemovedFromWorld(ULevel* InLevel, UWorld* InWorld);
 	
 };
