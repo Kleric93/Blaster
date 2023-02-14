c4f1272388dc9243e5c672fbcd3f825318e5e4ee
Kleric93 <121462973+Kleric93@users.noreply.github.com>
2023-02-14 12:41:59 +0100

    Lecture 60 Pitch in Multiplayer (fix aim offset  in multi)
    https://www.udemy.com/course/unreal-engine-5-cpp-multiplayer-shooter/learn/lecture/31457380#questions
    
    here we pretty much fixed the aim offset not visualized correctly when the server was looking at the client due to UE compressing the aim rotation to a different value (not negative), so we fixed it using this code, which will change it after compression to make it display properly

--- Source/Blaster/Character/BlasterCharacter.cpp
+++ Source/Blaster/Character/BlasterCharacter.cpp
@@ -190,6 +190,13 @@ void ABlasterCharacter::AimOffset(float DeltaTime)
 	}
 
 	AO_Pitch = GetBaseAimRotation().Pitch;
+	if (AO_Pitch > 90.f && !IsLocallyControlled()) //this is to fix the client looking like is aiming up all the time from the server's view
+	{
+		// map pitch from the range [270, 360) to [-90, 0)
+		FVector2D InRange(270.f, 360.f);
+		FVector2D OutRange(-90.f, 0.f);
+		AO_Pitch = FMath::GetMappedRangeValueClamped(InRange, OutRange, AO_Pitch);
+	}
 }
 
 void ABlasterCharacter::SetOverlappingWeapon(AWeapon* Weapon)
