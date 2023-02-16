9da924da4d21578801998c8f20cb03c23b48bd6d
Kleric93 <121462973+Kleric93@users.noreply.github.com>
2023-02-17 00:32:23 +0100

    Lecture 88 Shrink Crosshairs when Aiming
    https://www.udemy.com/course/unreal-engine-5-cpp-multiplayer-shooter/learn/lecture/31514960#questions

--- Source/Blaster/BlasterComponents/CombatComponent.cpp
+++ Source/Blaster/BlasterComponents/CombatComponent.cpp
@@ -111,7 +111,23 @@ void UCombatComponent::SetHUDCrosshairs(float DeltaTime)
 				CrosshairInAirFactor = FMath::FInterpTo(CrosshairInAirFactor, 0.f, DeltaTime, 30.f);
 			}
 
-			HUDPackage.CrosshairSpread = CrosshairVelocityFactor + CrosshairInAirFactor;
+			if (bAiming)
+			{
+				CrosshairAimFactor = FMath::FInterpTo(CrosshairAimFactor, 0.58f, DeltaTime, 30.f);
+			}
+			else
+			{
+				CrosshairAimFactor = FMath::FInterpTo(CrosshairAimFactor, 0.f, DeltaTime, 30.f);
+			}
+
+			CrosshairShootingFactor = FMath::FInterpTo(CrosshairShootingFactor, 0.f, DeltaTime, 20.f);
+
+			HUDPackage.CrosshairSpread = 
+				0.5f +
+				CrosshairVelocityFactor + 
+				CrosshairInAirFactor - 
+				CrosshairAimFactor + 
+				CrosshairShootingFactor;
 
 			HUD->SetHUDPackage(HUDPackage);
 		}
@@ -173,6 +189,11 @@ void UCombatComponent::FireButtonPressed(bool bPressed)
 		FHitResult HitResult;
 		TraceUnderCrosshairs(HitResult);
 		ServerFire(HitResult.ImpactPoint);
+
+		if (EquippedWeapon)
+		{
+			CrosshairShootingFactor += 1.f;
+		}
 	}
 }
 
