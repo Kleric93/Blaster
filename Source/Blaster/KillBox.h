0a43e96eca46ae4d6ae7591e4177130b001ced45
Kleric93 <121462973+Kleric93@users.noreply.github.com>
2023-02-23 22:34:01 +0100

    assets load (M4AZ) and other stuff

---
+++ Source/Blaster/KillBox.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/KillZVolume.h"
+#include "KillBox.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BLASTER_API AKillBox : public AKillZVolume
+{
+	GENERATED_BODY()
+	
+};
