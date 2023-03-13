74b2caa2b81406b34ebd8b4c03f353cbfce00e5e
Kleric93 <121462973+Kleric93@users.noreply.github.com>
2023-03-13 18:22:12 +0100

    Lecture 200 Shotgun Score Request
    https://www.udemy.com/course/unreal-engine-5-cpp-multiplayer-shooter/learn/lecture/32142888#questions

--- Source/Blaster/BlasterComponents/LagCompensationComponent.h
+++ Source/Blaster/BlasterComponents/LagCompensationComponent.h
@@ -80,6 +80,13 @@ public:
 		const FVector_NetQuantize& HitLocation, 
 		float HitTime);
 
+	FShotgunServerSideRewindResult ShotgunServerSideRewind(
+		const TArray<ABlasterCharacter*>& HitCharacters,
+		const FVector_NetQuantize& TraceStart,
+		const TArray<FVector_NetQuantize>& HitLocations,
+		float HitTime
+	);
+
 	UFUNCTION(Server, Reliable)
 	void ServerScoreRequest(
 		ABlasterCharacter* HitCharacter,
@@ -87,6 +94,13 @@ public:
 		const FVector_NetQuantize& HitLocation,
 		float HitTime,
 		class AWeapon* DamageCauser);
+
+	UFUNCTION(Server, Reliable)
+		void ShotgunServerScoreRequest(
+			const TArray<ABlasterCharacter*>& HitCharacters,
+			const FVector_NetQuantize& TraceStart,
+			const TArray<FVector_NetQuantize>& HitLocations,
+			float HitTime);
 
 protected:
 
@@ -112,13 +126,6 @@ protected:
 	//
 	/// Shotgun
 	//
-
-	FShotgunServerSideRewindResult ShotgunServerSideRewind(
-		const TArray<ABlasterCharacter*>& HitCharacters, 
-		const FVector_NetQuantize& TraceStart,
-		const TArray<FVector_NetQuantize>& HitLocations,
-		float HitTime
-		);
 
 	FShotgunServerSideRewindResult ShotgunConfirmHit(
 		const TArray<FFramePackage>& FramePackages,
