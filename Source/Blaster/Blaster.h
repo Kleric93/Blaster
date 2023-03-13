0047ee8d7d685c874491d9464e360255523682ac
Kleric93 <121462973+Kleric93@users.noreply.github.com>
2023-03-14 00:53:24 +0100

    Lecture 205 Hit Box Collision Type
    https://www.udemy.com/course/unreal-engine-5-cpp-multiplayer-shooter/learn/lecture/32182614#questions

--- Source/Blaster/Blaster.h
+++ Source/Blaster/Blaster.h
@@ -5,6 +5,7 @@
 #include "CoreMinimal.h"
 
 #define ECC_SkeletalMesh ECollisionChannel::ECC_GameTraceChannel1
+#define ECC_HitBox ECollisionChannel::ECC_GameTraceChannel2
 
 #define EPS_Grass EPhysicalSurface::SurfaceType1
 #define EPS_Metal EPhysicalSurface::SurfaceType2
