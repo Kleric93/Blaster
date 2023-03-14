8bcdde6654e0c75e995dfce202eff75d5debd6cb
Kleric93 <121462973+Kleric93@users.noreply.github.com>
2023-03-14 15:13:17 +0100

    Lecture 213 Return To Main menu
    https://www.udemy.com/course/unreal-engine-5-cpp-multiplayer-shooter/learn/lecture/32241298#questions

--- Source/Blaster/Blaster.Build.cs
+++ Source/Blaster/Blaster.Build.cs
@@ -8,7 +8,7 @@ public class Blaster : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "UMG", "PhysicsCore", "Niagara", "Slate", "SlateCore" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "UMG", "PhysicsCore", "Niagara", "Slate", "SlateCore", "MultiplayerSessions", "OnlineSubsystem", "OnlineSubsystemSteam" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
